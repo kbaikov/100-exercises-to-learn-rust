50959441ab43a4611bfde0453a9cc98f195bf789
kbaikov <konstantin.baikov@gmail.com>
2025-04-08 21:37:30 +0200

    Finish chapter 4

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,91 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub struct SaturatingU16 {
+    pub value: u16,
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        SaturatingU16 { value }
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        SaturatingU16 { value: *value }
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        SaturatingU16 {
+            value: value as u16,
+        }
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        SaturatingU16 {
+            value: *value as u16,
+        }
+    }
+}
+
+impl SaturatingU16 {
+    fn new(value: u16) -> Self {
+        Self { value }
+    }
+}
+
+impl std::ops::Add for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        Self::new(self.value.saturating_add(rhs.value))
+    }
+}
+
+impl std::ops::Add<&SaturatingU16> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: &Self) -> Self {
+        Self::new(self.value.saturating_add(rhs.value))
+    }
+}
+impl std::ops::Add<u16> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: u16) -> Self {
+        Self::new(self.value.saturating_add(rhs))
+    }
+}
+
+impl std::ops::Add<&u16> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: &u16) -> Self {
+        Self::new(self.value.saturating_add(*rhs))
+    }
+}
+
+impl std::ops::Add<u8> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: u8) -> Self {
+        Self::new(self.value.saturating_add(rhs as u16))
+    }
+}
+
+impl std::ops::Add<&u8> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: &u8) -> Self {
+        Self::new(self.value.saturating_add(*rhs as u16))
+    }
+}
+
+// This was not obvious from the error message,
+// so copied this from the solution branch
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
+    }
+}
